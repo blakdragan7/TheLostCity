d2b1491f8c78d30dc96a9974eb5d5f08792194be
Jonathon Copeland <jcopela4@gmail.com>
2017-12-12 19:42:48 -0500

    restart

--- src/main/java/com/blakdragan7/TheLostCity/misc/TLCProxy.java
+++
@@ -1,8 +0,0 @@
-package com.blakdragan7.TheLostCity.misc;
-
-public abstract class TLCProxy 
-{
-	public static final String ProxyName = "com.blakdragan7.TheLostCity.misc.TLCProxy";
-	
-	public abstract void SidedEvent();
-}
